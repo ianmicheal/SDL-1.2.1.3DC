3806439423a2f108b4d460b992cb0723971adae1
Ian Micheal <59771322+ianmicheal@users.noreply.github.com>
2025-02-03 03:32:56 -0500

    Update SDL_dcvideo.c

--- src/video/dc/SDL_dcvideo.c
+++ src/video/dc/SDL_dcvideo.c
@@ -597,7 +597,7 @@ static void sdl_dc_blit_textured(void)
     }
 
     pvr_dr_init(&dr_state);
-    pvr_poly_cxt_txr(&cxt, PVR_LIST_OP_POLY, PVR_TXRFMT_RGB565|PVR_TXRFMT_NONTWIDDLED, sdl_dc_wtex, sdl_dc_htex, sdl_dc_memtex, PVR_FILTER_NEAREST);
+    pvr_poly_cxt_txr(&cxt, PVR_LIST_OP_POLY, PVR_TXRFMT_RGB565|PVR_TXRFMT_NONTWIDDLED, sdl_dc_wtex, sdl_dc_htex, sdl_dc_memtex, PVR_FILTER_NONE);//any filtering makes fps dip in software rendering
 
     hdr = (pvr_poly_hdr_t *)pvr_dr_target(dr_state);
     pvr_poly_compile(hdr, &cxt);
